eff0b773752edf8901055aebade1ad419769f51f
Nic Cope <negz@spotify.com>
2014-12-16 16:32:35 -0500

    Migrate to sp-pypi-upload

--- .spotify/pipelines/release.sh
+++ .spotify/pipelines/release.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -e
 
+# TODO(negz): Remove when https://ghe.spotify.net/supermario/buildbricks/pull/58 is merged
+export DEVPI_INDEX="https://pypi.spotify.net/spotify/production"
+
 if git describe --exact-match 2> /dev/null; then
-    VERSION=$(git describe)
+    export PBR_VERSION=$(git describe)
 else
-    VERSION=$(git describe | sed -E 's/([0-9]+)\.([0-9]+)-([0-9]+).*/\1.\2.\3/')
-    git tag -a "$VERSION" -m "Tagging version \"${VERSION}\""
+    export PBR_VERSION=$(git describe | sed -E 's/([0-9]+)\.([0-9]+)-([0-9]+).*/\1.\2.\3/')
 fi
 
-python setup.py sdist; scp -o StrictHostKeyChecking=no dist/*.tar.gz spotify-pypiserver@pypi.spotify.net:/var/lib/pypiserver/
+sp-pypi-upload
