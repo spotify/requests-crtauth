2c425dda461acae4d9a7f74d3c05153aad59e337
Steven Stone <steven@spotify.com>
2014-12-17 14:56:55 -0500

    Pass `--no-vcs' to `devpi upload' so we retain files.

--- .spotify/pipelines/release.sh
+++ .spotify/pipelines/release.sh
@@ -3,6 +3,8 @@
 # TODO(negz): Remove when https://ghe.spotify.net/supermario/buildbricks/pull/58 is merged
 export DEVPI_INDEX="https://pypi.spotify.net/spotify/production"
 
+export DEVPI_UPLOAD_ARGS="--no-vcs"
+
 if git describe --exact-match 2> /dev/null; then
     export PBR_VERSION=$(git describe)
 else
